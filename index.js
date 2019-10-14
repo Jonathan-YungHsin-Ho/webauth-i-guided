91502d7dbd33d5889823606dddd315c89996cee7
Jonathan Ho <johnnyho82@gmail.com>
2019-10-14 13:56:24 -0400

    Add protect middleware

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ server.get('/', (req, res) => {
 server.post('/api/register', (req, res) => {
   let user = req.body;
 
+  // validate the user
+
+  // hash the password
+  const hash = bcrypt.hashSync(user.password, 8);
+
+  // we override the password with the hash
+  user.password = hash;
+
   Users.add(user)
     .then(saved => {
       res.status(201).json(saved);
@@ -31,21 +39,25 @@ server.post('/api/register', (req, res) => {
 server.post('/api/login', (req, res) => {
   let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        } else {
+          res.status(401).json({ message: 'You cannot pass!!' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(400).json({ message: 'Please provide credentials' });
+  }
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', protected, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -54,19 +66,38 @@ server.get('/api/users', (req, res) => {
 });
 
 server.get('/hash', (req, res) => {
-  console.log(req.headers.password);
+  const password = req.headers.authorization;
 
-  const credentials = req.headers;
+  if (password) {
+    const hash = bcrypt.hashSync(password, 10); // the 10 is the number of rounds 2 ^ 8
+    // a good starting value is 14
 
-  const hash = bcrypt.hashSync(credentials.password, 14);
-
-  credentials.password = hash;
-
-  res.json({ password: credentials.password });
-  // read a password from Authorization header
-  // return an obj with password hashed using bcryptjs
-  // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
+    res.status(200).json({ hash });
+    // read a password from Authorization header
+    // return an obj with password hashed using bcryptjs
+    // { hash: '970(&(:OHKJHIY*HJKH(*^)*&YLKJBLKJGHIUGH(*P' }
+  } else {
+    res.status(400).json({ message: 'Please provide credentials' });
+  }
 });
+
+function protected(req, res, next) {
+  const username = req.headers.username;
+  const password = req.headers.password;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next();
+      } else {
+        res.status(401).json({ message: 'You cannot pass!!' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
